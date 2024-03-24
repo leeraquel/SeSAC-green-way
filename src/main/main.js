650634b6d054871eecc8bbf92539c7573c3066d6
leeraquel <thisisdayounglee@gmail.com>
2024-03-24 17:17:37 +0900

    feat: 다른 브랜드들 각자 다른 마커 표시 완료

--- src/main/main.js
+++ src/main/main.js
@@ -6,9 +6,9 @@ import {
 } from '../api/seoul.js';
 
 let nowLo = {
-  address: '서울 서대문구 대현동 110-4',
-  x: 126.942297119359,
-  y: 37.5587904603943,
+  address: '서울 서대문구 신촌역로 30 신촌민자역사',
+  x: 126.942387,
+  y: 37.5598242,
 };
 
 // 객체를 문자열로 변환하여 저장
@@ -23,55 +23,57 @@ sessionStorage.setItem(
 
 const storedAddress = JSON.parse(sessionStorage.getItem('address'));
 
-// // 저장된 주소의 좌표를 기반으로 지도의 기본 좌표 설정
-// var markerPosition = new kakao.maps.LatLng(storedAddress.y, storedAddress.x);
-// var marker = { position: markerPosition };
-// var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div
-//   staticMapOption = {
-//     center: new kakao.maps.LatLng(storedAddress.y, storedAddress.x), // 저장된 좌표로 중심 설정
-//     level: 3,
-//     marker: marker,
-//   };
-// // 이미지 지도를 생성합니다.
-// var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
-// 동적 지도를 생성하고 사용자 정의 마커 이미지를 적용하는 예제 코드
+// 사용자 정의 마커 이미지의 경로입니다.
+const userMarkerSrc = 'src/assets/icon/userMarker.svg';
+const seoulBikeMarkerImageSrc = 'src/assets/icon/seoulBikeMarker.svg';
+const kickgoingMarkerImageSrc = 'src/assets/icon/kickgoingMarker.svg';
+const elecleMarkerImageSrc = 'src/assets/icon/elecleMarker.svg';
 
+function createAndAddMarker(position, imageSrc, map) {
+  var imageSize = new kakao.maps.Size(36, 36); // 마커 이미지의 크기
+  var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+  var marker = new kakao.maps.Marker({
+    position: position,
+    image: markerImage,
+  });
+
+  marker.setMap(map);
+}
 // 지도 컨테이너를 선택합니다.
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+let mapContainer = document.getElementById('map'), // 지도를 표시할 div
   mapOption = {
     center: new kakao.maps.LatLng(storedAddress.y, storedAddress.x), // 지도의 중심좌표
     level: 3, // 지도의 확대 레벨
   };
 
 // 지도를 생성합니다.
-var map = new kakao.maps.Map(mapContainer, mapOption);
+let map = new kakao.maps.Map(mapContainer, mapOption);
 
-// 사용자 정의 마커 이미지의 경로입니다.
-var imageSrc = 'src/assets/icon/userMarker.svg';
+// 지도 생성 후 사용자의 현재 위치에 마커 추가
 
-// 마커 이미지의 이미지 크기 입니다.
-var imageSize = new kakao.maps.Size(35, 35); // 폭과 높이
+var userPosition = new kakao.maps.LatLng(storedAddress.y, storedAddress.x);
+createAndAddMarker(userPosition, userMarkerSrc, map);
 
-// 마커 이미지를 생성합니다
-var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-// 마커를 생성합니다
-var marker = new kakao.maps.Marker({
-  position: map.getCenter(), // 마커의 위치
-  image: markerImage, // 마커 이미지
+getSeoulBikeStatusWithin500m().then((stations) => {
+  stations.forEach((station) => {
+    var position = new kakao.maps.LatLng(
+      station.stationLatitude,
+      station.stationLongitude
+    );
+    createAndAddMarker(position, seoulBikeMarkerImageSrc, map);
+  });
 });
 
-// 마커가 지도 위에 표시되도록 설정합니다.
-marker.setMap(map);
-
-getSeoulBikeStatusWithin500m().then((nearbyStations) => {
-  console.log(nearbyStations); // 필터링된 대여소 정보 출력
+getKickgoingStatusWithin500m().then((locations) => {
+  locations.forEach((location) => {
+    var position = new kakao.maps.LatLng(location.y, location.x);
+    createAndAddMarker(position, kickgoingMarkerImageSrc, map);
+  });
 });
 
-getKickgoingStatusWithin500m().then((nearbyStations) => {
-  console.log(nearbyStations);
-});
-
-getElecleStatusWithin500m().then((nearbyStations) => {
-  console.log(nearbyStations);
+getElecleStatusWithin500m().then((locations) => {
+  locations.forEach((location) => {
+    var position = new kakao.maps.LatLng(location.y, location.x);
+    createAndAddMarker(position, elecleMarkerImageSrc, map);
+  });
 });

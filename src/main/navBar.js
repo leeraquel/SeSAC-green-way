df30541da79afe74f63f9b0d5021bc4ec4bf81c3
leeraquel <thisisdayounglee@gmail.com>
2024-03-25 14:57:56 +0900

    feat: navBar uitils 함수 따로 빼서 선언 적용 완료

--- src/main/navBar.js
+++ src/main/navBar.js
@@ -1,3 +1,4 @@
+// 로그인 여부 확인 후 마이페이지로 이동
 const isLoginMyPage = function () {
   var value = sessionStorage.getItem('islogin');
   if (value === 'true') {
@@ -7,6 +8,7 @@ const isLoginMyPage = function () {
     window.location.href = '../login/login.html';
   }
 };
+
 // 로그인 여부 확인 후 즐겨찾기 페이지로 이동
 const isloginFavoritePage = function () {
   var value = sessionStorage.getItem('islogin');
@@ -14,6 +16,6 @@ const isloginFavoritePage = function () {
     window.location.href = '../bookmark/favorite.html';
   } else {
     alert('로그인이 필요합니다.');
-    window.location.href = '../login/login.html';
+    window.location.href = '/src/login/login.html';
   }
 };

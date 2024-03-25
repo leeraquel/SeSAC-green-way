462fb23f519f370fb8d05b86aaafb5943f9882cf
Aningoo <aningooaningoo@Aningooui-noteubug.local>
2024-03-26 00:03:31 +0900

    fix:search/searchInput/api수정

--- src/search/search.js
+++ src/search/search.js
@@ -6,6 +6,8 @@ import {
 } from '../api/mapApi.js';
 
 let storedAddress
+const elecleURL = '../api/elecle.json';
+const kickgoingURL = '../api/kickgoing.json';
 
   // currentLocation
  async function getCurrentLocation() {
@@ -55,65 +57,86 @@ currentLocationBtn.addEventListener('click',getCurrentLocation)
 
 // 카카오 지도api
 if (window.kakao && kakao.maps) {
-  kakao.maps.load(function() {
-      // 장소 검색 객체 생성
-      var ps = new kakao.maps.services.Places(); 
+  kakao.maps.load(function () {
+    // 장소 검색 객체 생성
+    var ps = new kakao.maps.services.Places();
 
-      // 검색 버튼 클릭 이벤트 리스너 추가
-      document.getElementById('searchIcon').addEventListener('click', function() {
-          var keyword = document.getElementById('searchInput').value;
+    // 검색 버튼 클릭 이벤트 리스너 추가
+    document
+      .getElementById('searchIcon')
+      .addEventListener('click', function () {
+        var keyword = document.getElementById('searchInput').value;
 
-          if (!keyword.replace(/^\s+|\s+$/g, '')) {
-              alert('검색 키워드를 입력해주세요.');
-              return false;
-          }
+        if (!keyword.replace(/^\s+|\s+$/g, '')) {
+          alert('검색 키워드를 입력해주세요.');
+          return false;
+        }
 
-          // 키워드로 장소 검색 실행
-          ps.keywordSearch(keyword, placesSearchCB);
+        // 키워드로 장소 검색 실행
+        ps.keywordSearch(keyword, placesSearchCB);
       });
 
-      // 장소 검색 완료 시 호출되는 콜백 함수
-      function placesSearchCB(data, status) {
-          if (status === kakao.maps.services.Status.OK) {
-              // 검색 결과가 있으면 콘솔에 출력
-              console.log(data);
-              
-              // 검색 결과를 div에 표시 (예시)
-              var resultsDiv = document.getElementById('searchResults');
-              resultsDiv.innerHTML = ''; // 이전 결과 초기화
-              data.forEach(function(place) {
-                  var name = document.createElement('p');
-                  name.textContent = place.place_name; // 장소 이름 출력
-                  resultsDiv.appendChild(name);
+    // 장소 검색 완료 시 호출되는 콜백 함수
+    function placesSearchCB(data, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        // 검색 결과가 있으면 콘솔에 출력
+        console.log(data);
 
-                  name.addEventListener('click', async function() {
-                    let defaultLocation = {
-                      address: place.address_name,
-                      y: place.y,
-                      x: place.x,
-                    };
-            
-                    sessionStorage.setItem('address', JSON.stringify(defaultLocation));
-                    storedAddress = defaultLocation;
-                    document.getElementById("nowLocationInfo").style.display = 'block';
-                    document.getElementById('nowLocationName').textContent = storedAddress.address;
+        // 검색 결과를 div에 표시 (예시)
+        var resultsDiv = document.getElementById('searchResults');
+        resultsDiv.innerHTML = ''; // 이전 결과 초기화
+        data.forEach(function (place) {
+          var name = document.createElement('p');
+          name.textContent = place.place_name; // 장소 이름 출력
+          resultsDiv.appendChild(name);
 
-                    
-                    await fetchFavoritesAndUpdateIcon();
-                    
-                  })
+          name.addEventListener('click', async function () {
+            let defaultLocation = {
+              address: place.address_name,
+              y: place.y,
+              x: place.x,
+            };
 
-              }); 
-              
-          } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-              alert('검색 결과가 없습니다.');
-          } else if (status === kakao.maps.services.Status.ERROR) {
-              alert('검색 중 오류가 발생했습니다.');
-          }
+            sessionStorage.setItem('address', JSON.stringify(defaultLocation));
+            storedAddress = defaultLocation;
+            document.getElementById('nowLocationInfo').style.display = 'block';
+            document.getElementById('nowLocationName').textContent =
+              storedAddress.address;
+
+            await fetchFavoritesAndUpdateIcon();
+
+            const seoulBikeStations = await getSeoulBikeStatusWithin500m();
+            const totalSeoulBikes = seoulBikeStations.reduce(
+              (acc, station) => acc + parseInt(station.parkingBikeTotCnt, 10),
+              0
+            );
+            console.log(seoulBikeStations);
+            console.log(totalSeoulBikes);
+            const kickgoingStations = await getKickgoingStatusWithin500m(
+              kickgoingURL
+            );
+            console.log(kickgoingStations);
+
+            const elecleStations = await getElecleStatusWithin500m(elecleURL);
+
+            // 사용 가능 수량 HTML 업데이트
+            document.getElementById('seoulBikeCount').textContent =
+              totalSeoulBikes;
+            document.getElementById('kickgoingCount').textContent =
+              kickgoingStations.length;
+            document.getElementById('elecleCount').textContent =
+              elecleStations.length;
+          });
+        });
+      } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+        alert('검색 결과가 없습니다.');
+      } else if (status === kakao.maps.services.Status.ERROR) {
+        alert('검색 중 오류가 발생했습니다.');
       }
+    }
   });
 } else {
-  console.error("카카오 지도 JavaScript SDK가 로드되지 않았습니다.");
+  console.error('카카오 지도 JavaScript SDK가 로드되지 않았습니다.');
 }
 
 

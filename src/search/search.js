a7274e02e05768643c10635da5def1d091c1161e
leeraquel <thisisdayounglee@gmail.com>
2024-03-27 16:07:59 +0900

    feat: search 스타일 변경 및 로딩 모달 추가

--- src/search/search.js
+++ src/search/search.js
@@ -5,55 +5,58 @@ import {
   getElecleStatusWithin500m,
 } from '../api/mapApi.js';
 
-let storedAddress
+let storedAddress;
 const elecleURL = '../api/elecle.json';
 const kickgoingURL = '../api/kickgoing.json';
 
-  // currentLocation
- async function getCurrentLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            async function(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
+// currentLocation
+async function getCurrentLocation() {
+  document.getElementById('modal').style.display = 'block';
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      async function (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
 
-            // Google Maps Geocoding API 호출을 위한 URL
-            var geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleKey}&language=ko`;
+        // Google Maps Geocoding API 호출을 위한 URL
+        var geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleKey}&language=ko`;
 
-           try {
-                // Geocoding API 호출 및 결과 대기
-                const response = await fetch(geocodingUrl);
-                const data = await response.json();
-                const addressName = data.results[0].formatted_address;
-                console.log(data.results[0].formatted_address);
-      
-                // 세션 스토리지 업데이트
-                let currentLocation = {
-                  address: addressName, // 비동기 호출 결과로 얻은 주소 사용
-                  x: longitude,
-                  y: latitude,
-                }; 
+        try {
+          document.getElementById('modal').style.display = 'none';
+          // Geocoding API 호출 및 결과 대기
+          const response = await fetch(geocodingUrl);
+          const data = await response.json();
+          const addressName = data.results[0].formatted_address;
+          console.log(data.results[0].formatted_address);
+
+          // 세션 스토리지 업데이트
+          let currentLocation = {
+            address: addressName, // 비동기 호출 결과로 얻은 주소 사용
+            x: longitude,
+            y: latitude,
+          };
 
           sessionStorage.setItem('address', JSON.stringify(currentLocation));
           storedAddress = currentLocation;
 
-          const input = document.getElementById("searchInput");
-          input.value = "" + addressName;} catch (error) {
-            console.log('Geocoding API 호출 실패:', error);
-          }
-            
-        }, function() {
-            console.log('Geolocation service failed.');
-        });
-    } else {
-        console.log('Geolocation is not supported by this browser.');
-    }
+          const input = document.getElementById('searchInput');
+          input.value = '' + addressName;
+        } catch (error) {
+          console.log('Geocoding API 호출 실패:', error);
+        }
+      },
+      function () {
+        console.log('Geolocation service failed.');
+      }
+    );
+  } else {
+    console.log('Geolocation is not supported by this browser.');
+  }
 }
 
-const currentLocationBtn = document.getElementById("current-location-button")
+const currentLocationBtn = document.getElementById('current-location-button');
 
-
-currentLocationBtn.addEventListener('click',getCurrentLocation)
+currentLocationBtn.addEventListener('click', getCurrentLocation);
 
 // 카카오 지도api
 if (window.kakao && kakao.maps) {
@@ -74,30 +77,29 @@ if (window.kakao && kakao.maps) {
 
         // 키워드로 장소 검색 실행
         ps.keywordSearch(keyword, placesSearchCB);
-      
-      ps.keywordSearch(keyword, function(data, status, pagination) {
-        // 검색 결과가 있을 때
-        if (status === kakao.maps.services.Status.OK) {
+
+        ps.keywordSearch(keyword, function (data, status, pagination) {
+          // 검색 결과가 있을 때
+          if (status === kakao.maps.services.Status.OK) {
             // 검색 결과를 처리하는 코드
-            
+
             // nosearch 숨기기
-            document.querySelector(".nosearch").style.display = "none";
-        } else {
+            document.querySelector('.nosearch').style.display = 'none';
+          } else {
             // 검색 결과가 없을 때
             // nosearch 표시하기
-            document.querySelector(".nosearch").style.display = "block";
-        }
+            document.querySelector('.nosearch').style.display = 'block';
+          }
+        });
       });
-    });
     // 장소 검색 완료 시 호출되는 콜백 함수
     function placesSearchCB(data, status) {
       if (status === kakao.maps.services.Status.OK) {
         // 검색 결과가 있으면 콘솔에 출력
         console.log(data);
 
-
         // 검색 결과를 div에 표시 (예시)
-        var resultsDiv = document.getElementById('searchResults') ;
+        var resultsDiv = document.getElementById('searchResults');
         resultsDiv.innerHTML = ''; // 이전 결과 초기화
         data.forEach(function (place) {
           var name = document.createElement('p');
@@ -114,7 +116,8 @@ if (window.kakao && kakao.maps) {
             sessionStorage.setItem('address', JSON.stringify(defaultLocation));
             storedAddress = defaultLocation;
             document.getElementById('nowLocationInfo').style.display = 'block';
-            document.getElementById('nowLocationName').textContent = storedAddress.address;
+            document.getElementById('nowLocationName').textContent =
+              storedAddress.address;
             document.getElementById('resultBox').style.display = 'block';
 
             await fetchFavoritesAndUpdateIcon();
@@ -152,8 +155,6 @@ if (window.kakao && kakao.maps) {
 } else {
   console.error('카카오 지도 JavaScript SDK가 로드되지 않았습니다.');
 }
-
-
 
 async function fetchFavoritesAndUpdateIcon() {
   const currentLocation = JSON.parse(sessionStorage.getItem('address'));

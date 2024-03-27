987c9041fdb3623ea1b42c830e34c8f1bbc07f04
SOM-R2 <sjleee406@naver.com>
2024-03-27 14:30:16 +0900

    feat: 텝바 alert 지우기

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,14 @@
 const isLoginMyPage = function () {
-  var value = sessionStorage.getItem('islogin');
+  var value = sessionStorage.getItem('isLogin');
   if (value === 'true') {
     window.location.href = '../mypage/mypage.html';
   } else {
-    alert('로그인이 필요합니다.');
     window.location.href = '../login/login.html';
   }
 };
 // 로그인 여부 확인 후 즐겨찾기 페이지로 이동
 const isloginFavoritePage = function () {
-  var value = sessionStorage.getItem('islogin');
+  var value = sessionStorage.getItem('isLogin');
   if (value === 'true') {
     window.location.href = '../bookmark/favorite.html';
   } else {

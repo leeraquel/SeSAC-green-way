3af27a47fa9782d700039d886796d500c3fdb78c
leeraquel <thisisdayounglee@gmail.com>
2024-03-27 15:16:50 +0900

    feat: 현재 위치 로딩 시 얼럿뜨기

--- src/main/navBar.js
+++ src/main/navBar.js
@@ -4,13 +4,12 @@ const isLoginMyPage = function () {
   if (value === 'true') {
     window.location.href = '/src/mypage/mypage.html';
   } else {
-    alert('로그인이 필요합니다.');
     window.location.href = '/src/login/login.html';
   }
 };
 
 // 로그인 여부 확인 후 즐겨찾기 페이지로 이동
-const isloginFavoritePage = function () {
+const isLoginFavoritePage = function () {
   var value = sessionStorage.getItem('islogin');
   if (value === 'true') {
     window.location.href = 'src/bookmark/favorite.html';

3c2fcbd5cb8cda7121066b5f9be9013df1061f6d
leeraquel <thisisdayounglee@gmail.com>
2024-03-27 15:52:47 +0900

    style: myPage 스타일변경

--- src/main/navBar.js
+++ src/main/navBar.js
@@ -1,6 +1,6 @@
 // 로그인 여부 확인 후 마이페이지로 이동
 const isLoginMyPage = function () {
-  var value = sessionStorage.getItem('islogin');
+  var value = sessionStorage.getItem('isLogin');
   if (value === 'true') {
     window.location.href = '/src/mypage/mypage.html';
   } else {
@@ -10,7 +10,7 @@ const isLoginMyPage = function () {
 
 // 로그인 여부 확인 후 즐겨찾기 페이지로 이동
 const isLoginFavoritePage = function () {
-  var value = sessionStorage.getItem('islogin');
+  var value = sessionStorage.getItem('isLogin');
   if (value === 'true') {
     window.location.href = 'src/bookmark/favorite.html';
   } else {
